9dc93565ef2557a795491ed6b2b0b2940c98d3ac
mohak-savla <mohaksavla.law@gmail.com>
2024-04-15 06:01:41 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This line covers all JavaScript and JSX files in your src directory
+  ],
+  // other configurations...
+}
+
